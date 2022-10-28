7b831bddd27515a2c3de2cda11b016041b38f767
Maykon Eduardo Pereira de Sousa <maeduardo@uolinc.com>
2022-10-28 15:57:05 -0300

    valida usuário

--- src/use-cases/Users/CreateUserUseCase/CreateUserUseCase.ts
+++ src/use-cases/Users/CreateUserUseCase/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { inject, injectable } from "tsyringe";
 import { IDataUserModel, IUserCreateData, IUsersRepository } from "../../../repositories/UsersRepository";
 
@@ -10,11 +11,18 @@ export class CreateUserUseCase {
   ){}
 
   async execute({username}: IUserCreateData): Promise<IDataUserModel> {
-
+    
     if(!username) throw new Error("Username is required");
+    
     const userAlreadyExists = 
     await this.usersRepository.getByUsername(username) 
+
+    const url = `https://skylab-api.rocketseat.com.br/public/event/nlw-copa/referral/${username}`
+      
+    const  userIsValid = await axios.get(url).then(response => response.data.totalCount).catch(() => null);
+    
     if(userAlreadyExists) throw new Error("User already exists");
+    if(!userIsValid) throw new Error("User is not valid");
 
 
     const user = await this.usersRepository.create({
